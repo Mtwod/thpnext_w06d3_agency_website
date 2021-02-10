f905156bbc58b7135320b88e7179dcb653c31c6b
Mtwod <maximewong@hotmail.com>
2021-02-10 10:15:45 +0100

    Initial commit with eslint and sass

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import './style.scss';
+
+const App = () => (
+  <div className="App">
+    <h1>Hello World</h1>
+    <p>This is a starter for React app, place your components here!</p>
+  </div>
+);
+
+export default App;

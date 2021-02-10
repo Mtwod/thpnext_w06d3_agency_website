248424300ad6475b5b8bc3f98cb62d5ed86361b1
Mtwod <maximewong@hotmail.com>
2021-02-10 18:03:36 +0100

    Add author to studyCase data

--- src/data/studyCases.js
+++ src/data/studyCases.js
@@ -1,15 +1,18 @@
 const studyCases = [
   {
+    author: 'Platon',
     slug: 'platon-study-case',
     title: 'Le challenge',
     description: 'Platon a décidé de se lancer à l’époque malgré une crise économique difficile.\nNous avons été derrière eux pour leur apporter le meilleur du Web et du digital. Nous sommes vraiment la meilleure agence.',
   },
   {
+    author: 'Solane',
     slug: 'solane-study-case',
     title: 'Solane est le premier vendeur de fraises du Poitou-Charentes',
     description: 'Et c’est grâce à nous. Dans les moments les meilleurs comme les plus durs, nos campagnes de communication sur la place du marché ont fonctionné.',
   },
   {
+    author: 'Sedal',
     slug: 'sedal-study-case',
     title: 'Sedal, l’entreprise qui a commencé tout en bas… Et qui est toujours tout en bas',
     description: 'Malgré nos conseils et notre site web créé sur Wix, Sedal semble ne pas vouloir décoller. Mais comme un grand homme et une grande femme l’ont dit, "les derniers seront les premiers".',

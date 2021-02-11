2c335a6a57d5a7ac0b930a8e6209bf54ecc7463f
Mtwod <maximewong@hotmail.com>
2021-02-11 17:18:54 +0100

    Fix display client's name for list element

--- src/components/ClientListElement/index.jsx
+++ src/components/ClientListElement/index.jsx
@@ -6,7 +6,7 @@ const ClientListElement = () => (
   <ul className="ClientListElement">
     { studyCases.map((studyCase) => (
       <li className="ClientListElement__element" key={studyCase.slug}>
-        <Link to={`/works/${studyCase.slug}`} className="ClientListElement__element__link">{studyCase.title}</Link>
+        <Link to={`/works/${studyCase.slug}`} className="ClientListElement__element__link">{studyCase.author}</Link>
       </li>
     )) }
   </ul>
